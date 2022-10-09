1af985bdc6982bf3fcd16476fdb205f61261c89d
UdaHarry <harryrahmansyah97@gmail.com>
2022-10-09 11:37:04 +0700

    Setting React Proxy untuk hit Api Spring

--- src/main/react-ui/src/App.js
+++ src/main/react-ui/src/App.js
@@ -1,11 +1,27 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [getHello, setHello] = useState();
+
+  const hello = () => {
+    fetch('/api/hello')
+      .then(response => response.text())
+      .then(message => {
+        setHello(message);
+      })
+  }
+
+  useEffect(() => {
+    hello();
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>{getHello}</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
